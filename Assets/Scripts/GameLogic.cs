0f239874544896bddbfae1fce646070c7c8daab2
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2025-06-07 09:56:20 +0800

    完成基本逻辑，待完成map生成

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -14,8 +15,13 @@ public class GameLogic : MonoBehaviour
     [SerializeField]
     private float offsetSize = 85f;
     [SerializeField]
+    private float barOffset = 115f; // Offset for the bar position
+    [SerializeField]
     private RectTransform targetRectTransform;
     private Pool<Cell> cellPool;
+    private List<Cell> cells = new();
+    private bool isFailed;
+
     void Start()
     {
         transform.localPosition = Vector3.zero; // Reset position to origin
@@ -32,7 +38,6 @@ public class GameLogic : MonoBehaviour
         // float centerX = (col - 1) * offsetSize / 2f;
         // float centerY = (row - 1) * offsetSize * 1.2f / 2f;
         // float centerZ = (layer - 1) * offsetSize / 2f / 2f;
-
         for (int i = 0; i < layer; i++)
         {
             int curRow = row - i;
@@ -58,17 +63,95 @@ public class GameLogic : MonoBehaviour
                     );
                     cell.name = $"Cell_{i}_{j}_{k}";
                     cell.MouseEnabled = true; // Enable mouse interaction
+                    cell.Value = Random.Range(1, 15); // Random value between 1 and 13
                     cell.OnCellClicked.AddListener(() =>
                     {
-                        cell.transform.DOLocalMove(targetRectTransform.localPosition, 0.5f)
-                            .SetEase(Ease.OutBack)
-                            .OnComplete(() =>
-                            {
-                                // Add the logic to move the cell to the target position
-                            });
+                        AddCellToBar(cell);
+                        // cell.transform.DOLocalMove(targetRectTransform.localPosition, 0.5f)
+                        //     .SetEase(Ease.OutBack)
+                        //     .OnComplete(() =>
+                        //     {
+                        //         //TODO: Add the logic to move the cell to the target position
+                        //         AddCellToBar(cell);
+                        //     });
                     });
                 }
             }
+        }
+    }
+
+/// <summary>
+/// Update the position of cells on the bar.
+/// </summary>
+/// <param name="idx">the index of the cell that will be move</param>
+    private void UpdateCellOnBar(int idx = 0)
+    {
+        for (int i = idx; i < cells.Count; i++)
+        {
+            var cell = cells[i];
+            cell.transform.DOLocalMoveX(
+                i * barOffset, 0.5f
+            );
+        }
+    }
+
+    private void AddCellToBar(Cell cell)
+    {
+        bool isAdd = false;
+        cell.transform.SetParent(targetRectTransform);
+        for (int i = 0; i < cells.Count; i++)
+        {
+            Cell c = cells[i];
+            if (c.Value == cell.Value)
+            {
+                if (i < cells.Count - 1)
+                {
+                    if (cells[i + 1].Value == cell.Value)
+                    {
+                        UpdateCellOnBar(i + 2);
+                        var a = cells[i];
+                        var b = cells[i + 1];
+                        cells.RemoveRange(i, 2);
+                        cell.transform.DOLocalMove(
+                            new Vector3(b.transform.localPosition.x + barOffset, 0, 0), 0.5f
+                        ).SetEase(Ease.OutBack).OnComplete(() =>
+                        {
+                            cellPool.ReturnObject(cell);
+                            cellPool.ReturnObject(a);
+                            cellPool.ReturnObject(b);
+                            AudioManager.Instance.PlaySFX(2);
+                            UpdateCellOnBar();
+                        });
+
+                        return;
+                    }
+                    else
+                    {
+                        cells.Insert(i + 1, cell);
+                        isAdd = true;
+                        break;
+                    }
+
+                }
+            }
+        }
+        if (!isAdd)
+        {
+            cells.Add(cell);
+        }
+
+
+        cell.transform.DOLocalMove(new Vector3(
+            cells.Count * barOffset, 0, 0
+        ), 0.5f).SetEase(Ease.OutBack);
+        cell.MouseEnabled = false; // Disable mouse interaction after adding to bar
+
+        UpdateCellOnBar();
+
+        if (cells.Count >= 7)
+        {
+            Debug.Log($"Game Over!");
+            isFailed = true;
         }
     }
 }
